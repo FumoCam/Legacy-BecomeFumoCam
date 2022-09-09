4594f8ad173552ca6508da0d6accf717e9a72daa
FumoCam <97715113+FumoCam@users.noreply.github.com>
2022-09-09 19:23:06 -0400

    Fix "initalize serial" on boot

--- resources/windows_fumocam_startup.bat
+++ resources/windows_fumocam_startup.bat
@@ -5,7 +5,7 @@ cd %USERPROFILE%\Desktop\FumoCam\src\
 echo [System Reboot Detected]> ..\output\main_process.txt
 echo Please wait. Initializing core systems. (0/2)> ..\output\main_status.txt
 TIMEOUT /T 15
-start /wait initialize_serial.py
+start /wait poetry run python initialize_serial.py
 echo [System Reboot Detected]> ..\output\main_process.txt
 echo Please wait. Initializing core systems. (1/2)> ..\output\main_status.txt
 TIMEOUT /T 10
